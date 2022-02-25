a05eb4e840e9559973235eb4d7074e415c3b3150
Raju14321 <100340131+Raju14321@users.noreply.github.com>
2022-02-25 11:48:52 +0530

    Add files via upload

---
+++ MultiInterface/Interface2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiInterface
+{
+    interface Interface2
+    {
+        void Test();
+        
+        void Show();
+    }
+}
---
+++ MultiInterface/MulClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MultiInterface
+{
+    
+    class MulClass:Interface1,Interface2
+    {
+       public void Test()
+       {
+            Console.WriteLine("This are pravite property");
+       }
+
+        void Interface1.Show()
+        {
+            Console.WriteLine("1 Show Raju");
+        }
+
+        void Interface1.Test()
+        {
+            Console.WriteLine("1 Test Raju");
+        }
+
+
+        void Interface2.Show()
+        {
+            Console.WriteLine("2 show Lucky");
+        }
+
+       
+        void Interface2.Test()
+        {
+            Console.WriteLine("2 Test Lucky");
+        }
+        static void Main()
+        {
+            MulClass m = new MulClass();
+            m.Test();
+            Interface1 o = m;
+            o.Test();
+            o.Show();
+            Interface2 p = m;
+            p.Show();
+            p.Test();
+            Console.ReadLine();
+        }
+    }
+     
+    
+}
